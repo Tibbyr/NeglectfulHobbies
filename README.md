52716c434cbaaa79da72ff97ea215d94a262170e
J270697 <J270697@tafe.wa.edu.au>
2025-07-01 21:43:00 +0800

    feat: Added README.md file, expanded TODO list, added main functions

---
+++ README.md
@@ -0,0 +1,33 @@
+# Neglectful Hobbies
+
+## Intro:
+
+I wanna make something for my raspberry pi, so I'm making a todo list app which sends me annoying notifications to do my to's when I shoo.
+
+## Problems to solve (Scope):
+
+1. Calendar/Todo apps are too busy
+2. Not enough accountability
+
+## Why?
+Because...
+- Too much information clustered in too little space
+- No incentive to complete a task
+- No check ins
+- No pressure for approaching deadlines
+
+## So!
+I want to...
+- Ease the info dump: no more than the highest priority task showing
+  1. Scoring system for tasks (based on eisenhower matrix?)
+  2. Manual input allowed
+- User defined rewards OR points system (for gamification)
+- Notify for:
+  1. Upcoming deadlines
+     1. "xx:xx time left until {activity}"
+     2. "Start planning for {activity}?"
+  2. Missed deadlines 
+     1. "Did you do {activity}?"
+     2. "Reschedule {activity}?"
+     3. "Start {activity} now?"
+     4. "You missed date night with {activity}... call them back?"
--- src/main.py
+++ src/main.py
@@ -1,6 +1,35 @@
+import datetime
+import json
 # TODO: Write to file: Task to complete, deadline
 # TODO: Check if file created or initialised with expected values
+
+'''
+
+Wants: write and store tasks (could make sense to have these created as objects with classes)
+It should:
+- Record tasks and deadlines
+- Show upcoming deadlines on request (based on set or custom intervals)
+'''
+
+def todo_highest_item():
+    # TODO First: Load sorted data by highest score
+    with open("data.json", 'r') as file:
+        loaded_data = json.load(file)
+    return loaded_data
+
+def score_calc():
+    # TODO: Write score for item based on user defined score, or eisenhower matrix q&a
+    ...
+
+def main_screen():
+    # TODO: Display ordered todo list
+    name = input("What's your name again?: ")
+    print(f"Hi {name}, welcome back to Neglectful Hobbies")
+    first_todo = todo_highest_item()
+    print(f"Upcoming...\n {first_todo}")
+
+main_screen()
+
 # TODO: Calc Operations: Checks at set/custom intervals for upcoming deadlines
 # TODO: Hard - Send Operations: Notification to user in phone
-# TODO:
-# TODO:
+
