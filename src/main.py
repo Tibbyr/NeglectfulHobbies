6e3dff898f453c4f46639cd87bf59303abed6bc3
J270697 <J270697@tafe.wa.edu.au>
2025-07-01 13:27:06 +0800

    chore: First commit, create TODO list and src/tests folders

---
+++ src/main.py
@@ -0,0 +1,6 @@
+# TODO: Write to file: Task to complete, deadline
+# TODO: Check if file created or initialised with expected values
+# TODO: Calc Operations: Checks at set/custom intervals for upcoming deadlines
+# TODO: Hard - Send Operations: Notification to user in phone
+# TODO:
+# TODO:
